f3fe00d970570067f3b9b8e508e1246833344213
wfdsaz <niljungsang@gmail.com>
2018-08-28 19:51:59 +0900

    8월 28일

---
+++ Web/JSP/Jsp25_3/src/com/study/java/MemberAllService.java
@@ -0,0 +1,5 @@
+package com.study.java;
+
+public class MemberAllService {
+
+}
---
+++ Web/JSP/Jsp25_3/src/com/study/java/loginOk.java
@@ -0,0 +1,15 @@
+package com.study.java;
+
+import com.study.java.Service.Service;
+
+public class loginOk implements Service {
+
+	public loginOk() {
+		
+	}
+	@Override
+	public void execute(HttpServletRequest request, 
+			HttpServletResponse response)
+			throws ServletException, IOException ;
+	
+}
